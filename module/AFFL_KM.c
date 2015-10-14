1f3dec64164af58b21c224240be0e34ee894316d
Олег Цыганок <tsiganock.oleg@yandex.ru>
2015-10-14 17:01:27 +0300

    Init and release black list

--- module/AFFL_KM.c
+++ module/AFFL_KM.c
@@ -1,18 +1,19 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/init.h>
+#include "AFFL_list.h"
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("firewall");
 
 static int __init mod_init(void)
 {
-  
+  initBlackList();
 }
 
 static void __exit mod_exit(void)
 {
-  
+  releaseBlackList();
 }
 
 module_init(mod_init);
