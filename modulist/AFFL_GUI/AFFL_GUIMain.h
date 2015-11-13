658a687c23d5590bfc6ebdca7c855ac32a7b7112
Natali Karasevich <Stihia94@yandex.ru>
2015-11-14 00:47:02 +0200

    add work with list

---
+++ modulist/AFFL_GUI/AFFL_GUIMain.h
@@ -0,0 +1,88 @@
+/***************************************************************
+ * Name:      AFFL_GUIMain.h
+ * Purpose:   Defines Application Frame
+ * Author:     ()
+ * Created:   2015-11-05
+ * Copyright:  ()
+ * License:
+ **************************************************************/
+
+#ifndef AFFL_GUIMAIN_H
+#define AFFL_GUIMAIN_H
+
+//(*Headers(AFFL_GUIFrame)
+#include <wx/textdlg.h>
+#include <wx/grid.h>
+#include <wx/sizer.h>
+#include <wx/notebook.h>
+#include <wx/button.h>
+#include <wx/filedlg.h>
+#include <wx/panel.h>
+#include <wx/frame.h>
+#include <wx/timer.h>
+//*)
+#include "lists.h"
+
+class AFFL_GUIFrame: public wxFrame
+{
+    public:
+        AFFL_GUIFrame(wxWindow* parent,wxWindowID id = -1);
+
+        virtual ~AFFL_GUIFrame();
+
+    private:
+
+        wxString root_pass;
+
+        //(*Handlers(AFFL_GUIFrame)
+        void OnbtnKillClick(wxCommandEvent& event);
+        void OnbtnAddClick(wxCommandEvent& event);
+        void OnbtnAddPathClick(wxCommandEvent& event);
+        void OnbtnDelClick(wxCommandEvent& event);
+        void OntRefreshTrigger(wxTimerEvent& event);
+        void OnKillFocus(wxFocusEvent& event);
+        void OnSetFocus(wxFocusEvent& event);
+        void OnbtnDelClick1(wxCommandEvent& event);
+        void OnClose(wxCloseEvent& event);
+        //*)
+        void OngrdRangeSelect(wxGridRangeSelectEvent &event);
+
+        //(*Identifiers(AFFL_GUIFrame)
+        static const long ID_PROC_LIST;
+        static const long ID_ADD;
+        static const long ID_KILL;
+        static const long ID_PROC_PAN;
+        static const long ID_ADD_PATH;
+        static const long ID_DEL;
+        static const long ID_BLACK_PAN;
+        static const long ID_NOTEBOOK1;
+        static const long ID_REFRESH;
+        static const long ID_PASSWORDE;
+        //*)
+        static const long ID_BLACKLIST;
+
+        //(*Declarations(AFFL_GUIFrame)
+        wxFileDialog* fdAddByPath;
+        wxPanel* pnlBlacklist;
+        wxTimer tRefresh;
+        wxGrid* grdProcList;
+        wxButton* btnAddPath;
+        wxButton* btnKill;
+        wxButton* btnDel;
+        wxNotebook* Notebook1;
+        wxButton* btnAdd;
+        wxPasswordEntryDialog* pedPassword;
+        wxPanel* pnlProcList;
+        //*)
+        OpenListBox* lbBlacklist;
+
+        ProcList *proc_list;
+        BlackList *black_list;
+
+        DECLARE_EVENT_TABLE()
+
+    private:
+        bool initKernelModule();
+};
+
+#endif // AFFL_GUIMAIN_H
--- modulist/AFFL_filemodule.c
+++ modulist/AFFL_filemodule.c
@@ -12,11 +12,10 @@
 #include <linux/slab.h>
 #include <linux/list.h>
 #include <linux/seq_file.h>
-#include "AFFL_filemodule.h"
+#define MAX_STR_LEN 255
 #define PROCFS_NAME 		"blist"
-#define BlackList "/home/natali/modu/AFFL_blacklist"
-#define MYDIRPROC "Firewall"
-#define MYPROC "/proc/Firewall/blist"
+#define MYDIRPROC "affl"
+#define MYPROC "/proc/affl/blist"
 
 #define DEBUG
 
@@ -29,24 +28,47 @@
 
 static struct proc_dir_entry *Our_Proc_Dir;
 static struct proc_dir_entry *Our_Proc_File;
-static struct file *fileproc;
+
+typedef struct TBlackList
+{
+	 char* inf; 
+	 struct list_head list; /* kernel's list structure */
+}TBlackList;
+
+TBlackList mlist;
+
+
+char *buf;
+
 /**
  * The buffer used to store character for this module
  *
  */
 static char* procfs_buffer;
-static int last_pos;
 
-/**
- * The size of the buffer
- *
- */
-char *buf;
 /** 
  * This function is called then the /proc file is read
  *
  */
- 
+int findProcAndDel(long pos)
+{
+	TBlackList *elem;
+	long counter=0;
+	list_for_each_entry(elem, &mlist.list, list)
+    	{
+		if(pos==counter)
+		{
+			 printk(KERN_INFO "deleted %s", elem->inf);
+			 list_del(&elem->list);
+        		 kfree(elem);
+			 return 1;
+		}
+		counter++;	
+	}
+
+	return 0;
+} 
+
 int read_callback(struct seq_file *m, void *v)
 {
         seq_printf(m,"%s\n",procfs_buffer);
@@ -61,24 +83,33 @@ int read_callback(struct seq_file *m, void *v)
 
 size_t write_callback(struct file* file, const char __user* buffer, size_t count, int* pos)
 {
-	int res;
-    	if(last_pos==0)
-	{
-		if(copy_from_user(&procfs_buffer[*pos], buffer, count))
-		{
-              	  return -EFAULT;
-		}
-	}
+	TBlackList *elem;
+	long res;
+	int i=0;
+	char mystr[10];
+	buf=(char*) kmalloc(sizeof(char)*MAX_STR_LEN,GFP_USER);
 	
-	else
+	if(buffer[0]!='k')
 	{
-		if(copy_from_user(&procfs_buffer[last_pos], buffer, count))
-			 return -EFAULT;
-		res = (int)(count+last_pos);
-		last_pos= res;
-		
+		buf[count]='\0';
+		memcpy(buf,buffer,count);
+		elem=(TBlackList*)kmalloc(sizeof(TBlackList),GFP_KERNEL);
+		elem->inf=buf;
+		//INIT_LIST_HEAD(&elem->list);
+		list_add_tail(&(elem->list),&(mlist.list));
+		printk(KERN_INFO "add: %s", elem->inf);
 	}
-   
+	else
+	{	while(i<=count)
+		{
+			mystr[i] = buffer[i+1];
+			i++;
+		}
+		mystr[count-1]='\0';
+		kstrtol(mystr,10,&res);
+		if(findProcAndDel(res)==1)
+			printk(KERN_INFO "Element deleted");
+	}
      return count; // Return number of bytes passed by the user
 }
 
@@ -99,101 +130,62 @@ static const struct file_operations proc_file_fops = {
 
 int initBlackList(void)
 {
-	struct file *fileread;
-	unsigned long count;
-	mm_segment_t fs;
-	loff_t pos;
 	size_t buf_len=4096;
-	fileread=filp_open(BlackList,O_RDWR|O_CREAT,0644);
-	if (IS_ERR(fileread))
-	{
-   		return -EBADF;
-	}
-
-	buf = (char*) kmalloc(buf_len, GFP_KERNEL);
-	if (buf == NULL)
-	{
- 	   filp_close(fileread, 0);
-  	   return -ENOMEM;
-	} 
-
-	Our_Proc_Dir = proc_mkdir(MYDIRPROC,NULL);
-	Our_Proc_File = proc_create(PROCFS_NAME, 0644, Our_Proc_Dir,&proc_file_fops);
 	
-	if (Our_Proc_File == NULL) {
+	Our_Proc_Dir = proc_mkdir(MYDIRPROC,NULL);
+	Our_Proc_File = proc_create(PROCFS_NAME, 0666, Our_Proc_Dir,&proc_file_fops);
+	
+	if (Our_Proc_File == NULL) 
+	{
 		remove_proc_entry(PROCFS_NAME, NULL);
 		printk(KERN_ALERT "Error: Could not initialize /proc/%s\n",
 			PROCFS_NAME);
 		return -ENOMEM;
 	}
 
-	fs = get_fs();
-	set_fs(KERNEL_DS);
-	pos=0;
-	count=vfs_read(fileread,buf,buf_len,&pos);
-	buf[pos] = '\0';
-	fileproc=filp_open(MYPROC,O_RDWR,0);
-	if (IS_ERR(fileproc))
-	{
-		printk("File don't open in /proc\n");
-   		return -EBADF;
-	}
-
 	procfs_buffer = (char*) kmalloc(buf_len, GFP_KERNEL);
 	if (procfs_buffer == NULL)
 	{
- 	   filp_close(fileproc, 0);
   	   return -ENOMEM;
 	} 
+	INIT_LIST_HEAD(&mlist.list); 
 
-	last_pos=0;
-	vfs_write(fileproc,buf,buf_len,&last_pos);
-	kfree(buf);
-	filp_close(fileread,0);	
-	set_fs(fs);
-	last_pos=pos;
 	return 0;
 }
 
+
+
 int findProcInBlackList(const char*name)
 {
-	int pos;
-	int start;
-	int res;
-	pos=0;
-	start=0;
-
-	while(procfs_buffer[pos]!='\0')
-	{
-		if(procfs_buffer[pos]=='\n')
+	TBlackList *elem;
+	list_for_each_entry(elem, &mlist.list, list)
+    	{
+		if(strcmp(elem->inf,name) == 0)
 		{
-			res=strncmp(name,&procfs_buffer[start],pos-start);
-
-			if(res==0)
-			{
-			   printk("found:%i\n",start);
-
-			   return start;
-			}
-			start=pos+1;
+			printk(KERN_INFO "found string %s",name);
+			return 0;
 		}
-		pos++;
+		
 	}
-
-	return (-1);
+	return -1;
 }
-
 
 int releaseBlackList(void)
 {
-	filp_close(fileproc,0);
+	TBlackList *elem, *tmp;
+	list_for_each_entry_safe(elem, tmp, &mlist.list, list)
+    	{
+         	printk(KERN_INFO "freeing node %s\n", elem->inf);
+        	 list_del(&elem->list);
+        	 kfree(elem);
+  	 }
 	remove_proc_entry(PROCFS_NAME, Our_Proc_Dir);
 	remove_proc_entry(MYDIRPROC,NULL);
 	kfree(procfs_buffer);
-	
-
+	kfree(buf);
 	return 1;
 }
+
 EXPORT_SYMBOL(initBlackList);
 EXPORT_SYMBOL(findProcInBlackList);
 EXPORT_SYMBOL(releaseBlackList);
